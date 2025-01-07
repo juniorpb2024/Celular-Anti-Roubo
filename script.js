4dbac8d2748655876b713db679af62085d7fb9a5
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-07 17:36:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Loader Animation
+  // Elementos
+  const quadro1 = document.getElementById("quadro1");
+  const quadro2 = document.getElementById("quadro2");
   const progressText = document.getElementById("progress-text");
-  const circleProgress = document.querySelector(".circle-progress");
+  const fixNowButton = document.getElementById("fix-now");
+
+  // Variáveis
   let progress = 0;
 
+  // Loader Animation
   const interval = setInterval(() => {
     progress += 1;
     progressText.textContent = `${progress}%`;
 
     if (progress === 100) {
       clearInterval(interval);
+      quadro1.classList.remove("active");
+      quadro2.classList.add("active");
     }
-  }, 80); // Total 8 seconds
+  }, 80); // Total de 8 segundos
 
-  // Botão piscante redirecionando
-  const fixNowButton = document.getElementById("fix-now");
+  // Redirecionamento do botão
   fixNowButton.addEventListener("click", () => {
     window.location.href = "https://www.paguemenos.com.br";
   });
+
+  // Inicializar primeira página
+  quadro1.classList.add("active");
 });
