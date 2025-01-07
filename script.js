fb42332380080c7daea91d2ddf46cf831d3dbd97
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-07 20:08:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const quadro1 = document.getElementById("quadro1");
   const quadro2 = document.getElementById("quadro2");
   const progressText = document.getElementById("progress-text");
-  const progressBar = document.querySelector(".progress-bar");
   const fixNowButton = document.getElementById("fix-now");
   const loader = document.querySelector(".loader");
   const warnings = document.querySelector(".warnings");
@@ -24,7 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const interval = setInterval(() => {
     progress += 1;
     progressText.textContent = `${progress}%`;
-    progressBar.style.width = `${progress}%`;
 
     if (progress === 100) {
       clearInterval(interval);
