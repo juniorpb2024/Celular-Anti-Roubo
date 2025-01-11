cd550fc995dd2dbd0ac825e002abf25a976be8a5
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 16:19:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function iniciarTeste() {
-    alert("Teste de segurança iniciado! Em breve, você será redirecionado.");
-    // Redireciona para o site especificado
-    window.location.href = "https://www.google.com/intl/pt-br_br/business";
-}
