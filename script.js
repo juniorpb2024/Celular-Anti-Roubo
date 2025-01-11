e50788b325132722ddcdf7b2e41d921c69500e35
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 15:55:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function iniciarTeste() {
-    // Redireciona para a nova página
-    window.location.href = "news.html";
-}
-
-function redirecionarParaGoogle() {
-    // Redireciona para o Google Business
     window.location.href = "https://www.google.com/intl/pt-br_br/business";
 }
