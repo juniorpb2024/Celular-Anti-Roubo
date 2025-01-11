775b86c762180bb5ed6b33ced67f7af37de0f232
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 13:24:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function iniciarTeste() {
-    alert("Teste de segurança iniciado! Em breve, você será redirecionado.");
-    // Substitua pelo redirecionamento ou ação desejada
-    window.location.href = "https://seusite.com/teste";
-}
+
