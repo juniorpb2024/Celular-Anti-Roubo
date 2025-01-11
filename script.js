39cf5e12a3754045b5f01b3c1d3c31a61561b46b
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 09:00:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const iniciarTesteButton = document.getElementById("iniciar-teste");
+  const progressText = document.getElementById("progress-text");
+  const progressBar = document.querySelector(".progress");
+  let progress = 0;
 
+  iniciarTesteButton.addEventListener("click", () => {
+    document.getElementById("primeira-pagina").classList.remove("active");
+    document.getElementById("quadro1").classList.add("active");
+
+    const interval = setInterval(() => {
+      progress += 1;
+      progressBar.style.width = `${progress}%`;
+      progressText.textContent = `${progress}%`;
+
+      if (progress === 100) {
+        clearInterval(interval);
+        document.getElementById("quadro1").classList.remove("active");
+        document.getElementById("quadro2").classList.add("active");
+      }
+    }, 80);
+  });
+
+  const randomPeople = Math.floor(Math.random() * (200 - 67 + 1)) + 67;
+  document.getElementById("people-online").textContent = randomPeople;
+});
