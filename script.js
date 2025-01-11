6b0d705c9634ca4644e607bc1de1ddeaee6746b8
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 14:28:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+function iniciarTeste() {
+    alert("Teste de segurança iniciado! Em breve, você será redirecionado.");
+    // Substitua pelo redirecionamento ou ação desejada
+    window.location.href = "https://www.paguemenos.com.br";
