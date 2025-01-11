283cd52323d9764bbd4992b02c806a12fa5c0480
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 14:58:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function iniciarTeste() {
+    // Redireciona para a próxima página
+    window.location.href = "next.html";
+}
 
+function continuar() {
+    alert("Continuando para o próximo passo...");
+    // Adicione o comportamento necessário para a etapa seguinte
+}
