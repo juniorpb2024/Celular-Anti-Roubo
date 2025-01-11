26bfbe4b97ca3f9010d4866dea81287b6c272759
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 16:10:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById('start-test').addEventListener('click', function() {
+function iniciarTeste() {
+    alert("Teste de segurança iniciado! Em breve, você será redirecionado.");
+    // Redireciona para o site especificado
     window.location.href = "https://www.google.com/intl/pt-br_br/business";
-});
+}
