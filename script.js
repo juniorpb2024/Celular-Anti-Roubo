25abf549dd1fe8fcbb370cc09d227c5509ac2e5d
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-11 14:52:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function navegarParaNoticia() {
-    document.getElementById("pagina-inicial").classList.add("hidden");
-    document.getElementById("noticia").classList.remove("hidden");
-}
 
-function continuar() {
-    alert("Você será redirecionado para a próxima etapa.");
-    window.location.href = "https://www.paguemenos.com.br";
-}
