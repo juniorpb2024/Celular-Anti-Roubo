9ad958ddfeb3c997a7160c48ee0c5d4a2e523ea1
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-13 17:56:18 -0300

    Create script-oitava.js

---
+++ script-oitava.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sliderThumb = document.getElementById("slider-thumb");
+    const sliderTrack = document.querySelector(".slider-track");
+    const sliderPercent = document.querySelector(".slider-percent");
+
+    let position = 0;
+    const targetPosition = 27;
+
+    const interval = setInterval(() => {
+        if (position >= targetPosition) {
+            clearInterval(interval);
+            sliderPercent.style.opacity = "1"; // Mostra o 27% quando a barra para
+        } else {
+            position++;
+            sliderThumb.style.left = `${position}%`;
+            sliderTrack.style.width = `${position}%`;
+        }
+    }, 20); // Atualiza a cada 20ms
+});
