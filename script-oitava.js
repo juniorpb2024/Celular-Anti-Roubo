92fe898c276d42785414b4a21f120f266c41eff6
juniorpb2024 <jcjuniorm23@gmail.com>
2025-01-15 08:36:45 -0300

    Update script-oitava.js

--- script-oitava.js
+++ script-oitava.js
@@ -1,20 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sliderThumb = document.getElementById("slider-thumb");
-    const sliderTrack = document.querySelector(".slider-track");
-    const sliderPercent = document.querySelector(".slider-percent");
+    const sliderTrack = document.getElementById("slider-track");
+    const tooltip = document.getElementById("tooltip");
 
-    let position = 0;
     const targetPosition = 27; // Posição final
-
-    const interval = setInterval(() => {
-        if (position >= targetPosition) {
-            clearInterval(interval); // Para o movimento ao atingir o alvo
-            sliderPercent.style.opacity = "1"; // Mostra o 27%
-            sliderPercent.style.left = `${position}%`; // Posiciona abaixo do ponto
-        } else {
-            position++;
-            sliderThumb.style.left = `${position}%`; // Move o ponto
-            sliderTrack.style.width = `${position}%`; // Move a barra
-        }
-    }, 20); // Atualiza a cada 20ms
+    sliderThumb.style.left = `${targetPosition}%`;
+    sliderTrack.style.width = `${targetPosition}%`;
+    tooltip.style.left = `${targetPosition}%`;
 });
